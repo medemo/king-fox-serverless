cc2a0e406aa5043d6d63e1c91d8ce4ed91e67d64
Armedi <armedi@outlook.com>
2020-01-22 11:47:45 +0700

    firebase function and lambda very basic example

---
+++ lambda/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.add = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'adding data',
+        data: event.body
+      },
+      null,
+      2
+    ),
+  };
+};
